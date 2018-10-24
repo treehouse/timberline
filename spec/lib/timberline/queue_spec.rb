1fbf246c0987dca7fd2bf8070c28c2d29b0edab3
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-10-24 00:08:32 -0700

    updated current tests to correct config to pass, all tests passing

--- spec/lib/timberline/queue_spec.rb
+++ spec/lib/timberline/queue_spec.rb
@@ -113,6 +113,9 @@ describe Timberline::Queue do
     let(:item) { subject.push("apple"); subject.pop }
 
     before do
+      Timberline.configure do |c|
+        c.log_job_result_stats = true
+      end
       subject.error_item(item)
     end
 
@@ -135,6 +138,9 @@ describe Timberline::Queue do
 
     context "when the item hasn't been retried before" do
       before do
+        Timberline.configure do |c|
+          c.log_job_result_stats = true
+        end
         subject.retry_item(item)
       end
 
@@ -153,6 +159,9 @@ describe Timberline::Queue do
 
     context "when the item has been retried before" do
       before do
+        Timberline.configure do |c|
+          c.log_job_result_stats = true
+        end
         item.retries = 3
         subject.retry_item(item)
       end
